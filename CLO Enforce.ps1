6927bb718ef0108ff968f6d5799a8d9eb0c4009a
brian-mcmullen <49528074+brian-mcmullen@users.noreply.github.com>
2021-09-28 10:04:07 -0400

    Add files via upload

---
+++ CLO Enforce.ps1
@@ -0,0 +1,8 @@
+﻿$searchbase = "OU=S-6,OU=Users,OU=CE,OU=MEUFWD,DC=MEUFWD,DC=usmc,DC=mil"
+
+$accountlist = "C:\CLOexemptaccounts.csv" 
+
+$cloexemptusers = Get-ADUser -SearchBase $searchbase -Filter {smartcardlogonrequired -eq $false} | fl Name,Description,Enabled,SmartCardLogonRequired
+$cloexemptusers | Export-Csv $accountlist -NoTypeInformation
+
+Get-ADUser -SearchBase $searchbase -Filter {smartcardlogonrequired -eq $false} | ForEach-Object {Set-ADUSer -Identity $_ -SmartcardLogonRequired $True} 
---
+++ Disable Inactive Accounts.ps1
@@ -0,0 +1,29 @@
+﻿$ButtonTypeEnd = [System.Windows.MessageBoxButton]::OK
+$MessageboxTitleEnd = "Disabled inactive accounts"
+$MessageboxbodyEnd = "The script has now finished running."
+$MessageIconEnd = [System.Windows.MessageBoxImage]::Information
+
+
+$Look_Here = "OU=MEUFWD,DC=MEUFWD,DC=usmc,DC=mil"
+
+$OutPath = "C:\"
+
+$Logdate = Get-date -f yyyy.MM.dd.hh.mm
+
+$ADDate = get-date -f yyyy.MM.dd
+
+$End = $OutPath + "u_ll_more_than_90$logdate.csv"
+
+
+
+$90dayusers = Get-ADUser -filter * -SearchBase $Look_Here -Properties * | where {
+            ($_.lastlogondate -lt (get-date ).AddDays(-90))}
+               
+$90dayusers | % {set-aduser -identity $_.samaccountname -Description "Disabled from lack of activity: $ADDate" -Enabled $false -SmartcardLogonRequired $true}
+
+
+$GrabInfo = $90dayusers | select Name, Description,Enabled,SmartCardLogonRequired
+
+$GrabInfo | Export-Csv $End -NoTypeInformation
+
+[System.Windows.MessageBox]::Show($MessageboxbodyEnd,$MessageboxTitleEnd,$ButtonTypeEnd,$MessageIconEnd)
